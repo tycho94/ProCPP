1faa78715475a284047adef0d5b7d8f0190c28c8
SeekerGit <sahar.bakhtiari@hotmail.com>
2015-12-03 14:30:12 +0100

    Update CoinWallet.ino

--- LaundryMachine/CoinWallet.ino
+++ LaundryMachine/CoinWallet.ino
@@ -3,6 +3,15 @@
 CoinWallet::CoinWallet(ICoin * c)
 {
   mCoin = c;
+  balance = 0;
+}
+void CoinWallet::showBalance()
+{
+  int remainder;
+  int q;
+  q= balance/200;
+  remainder=balance%200;
+  mCoin->Set
 }
 
 void CoinWallet::Poll()
@@ -11,11 +20,32 @@ void CoinWallet::Poll()
 
 int CoinWallet::Balance()
 {
-  return (0);
+  return balance;
 }
 
 boolean CoinWallet::Withdraw(int amount)
 {
+  if ( amount<=balance)
+  {
+    balance=balance-amount;
+    showBalance();
+    return true;
+  }
+  else
+  {
   return (false);
+  }
+}
+boolean CoinWallet::Deposit( int amount){
+  if( amount>0 && (balance+amount<=600) )
+  {
+    balance=balance+amount;
+    showBalance();
+    return true;
+  }
+  else
+  {
+    return false;
+  }
 }
 
