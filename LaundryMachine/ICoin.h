20ad803c1971f58f5a13dd77d242f49a38a1023b
SeekerGit <sahar.bakhtiari@hotmail.com>
2015-12-03 14:32:29 +0100

    Update ICoin.h

--- LaundryMachine/ICoin.h
+++ LaundryMachine/ICoin.h
@@ -9,9 +9,9 @@ class ICoin
     virtual boolean GetCoin50Button() = 0;
 
     // Outputs
-    virtual void SetCoin10(byte leds) = 0;
+    virtual void SetCoin10(int leds) = 0;
     virtual void SetCoin50(int leds) = 0;
-    virtual void SetCoin200(byte leds) = 0;
+    virtual void SetCoin200(int leds) = 0;
 };
 
 #endif
