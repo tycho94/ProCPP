91a845950acd0869781207d95dc4a106f16a648a
heil Biach <351256@student.fontys.nl>
2015-12-04 13:00:30 +0100

    test fixture coinwallet

---
+++ LaundryMachine/TTest/TestCoinWallet.cpp
@@ -0,0 +1,23 @@
+
+#include "../HardwareControl.h"
+#include "../ICoin.h"
+#include "../CoinWallet.h"
+
+
+TestCoinWallet::Coin200(){
+
+
+
+}
+
+TestCoinWallet::Coin50(){
+
+
+
+}
+
+TestCoinWallet::Coin10(){
+
+
+
+}
