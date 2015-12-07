eff44b7486b26ab47fd78b46a391f508fcb20e73
JustUser1410 <JustUser1410@users.noreply.github.com>
2015-12-07 19:31:24 +0100

    Some more work on IMotor and IWater
    IWater - by Apple

--- LaundryMachine/IMotor.h
+++ LaundryMachine/IMotor.h
@@ -21,11 +21,8 @@ class IMotor
     // Outputs
     virtual void SetDirection(int dir) = 0; //0=left 1=right
     virtual void SetMotorSpeed(int speed) = 0; //0=off 1=slow 2=medium 3=high
-    virtual void GetMotorSpeed();
   private:
     Speed motorSpeed;
 };
 
 #endif
-
-
--- LaundryMachine/IWater.h
+++ LaundryMachine/IWater.h
@@ -1,20 +1,25 @@
 #ifndef IWATER_H
 #define IWATER_H
 
+enum WaterLevel
+{
+    WATER_0_PERCENT,
+    WATER_33_PERCENT,
+    WATER_66_PERCENT,
+    WATER_100_PERCENT
+};
+
 class IWater
 {
   public:
     // Inputs
   virtual void SetSink(boolean state) = 0;
   virtual void SetDrain(boolean state) = 0;
-  virtual boolean GetWaterPressure()=0
 
     // Outputs
     virtual void SetWaterlevel(int level) = 0;
-    virtual int GetWaterLevel()=0;
+    virtual WaterLevel GetWaterLevel()=0;   //level: 0=empty, 1=1/3, 2=2/3, 3=filled
     virtual boolean GetPressureSwitch() = 0;
 };
 
 #endif
-
-
